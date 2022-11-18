4953b816c8cf69e27f819c85fc8416877e6b0356
deMartino1908303 <115620752+deMartino1908303@users.noreply.github.com>
2022-11-18 17:07:27 +0100

    Update Rabbits and Recurrence Relations.py

--- Rabbits and Recurrence Relations.py
+++ Rabbits and Recurrence Relations.py
@@ -1,6 +1,10 @@
 from data_reader import read_data
 info = read_data("rosalind_fib.txt").split()
-child = 1
-parent = 0
-for _ in range(1,int(info[0])) : child, parent = parent , parent + (child * int(info[1]))
-print(child)
+month = int(info[0])
+fertility = int(info[1])
+family = [1, 0]
+for __ in range(month):
+  tmp = family[0]
+  family[0] = family[1]*ferility
+  family[1] += tmp
+print(family[0])
